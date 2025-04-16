299b202e980f48adb8984af7a02b6df8f59b624c
Iago Fabricio Silva Melo <iagosilvamelo@gmail.com>
2025-04-16 11:44:28 -0300

    Aprimorado sessao de descricao da empresa

--- src/Components/Sections/DescriptionSection.vue
+++ src/Components/Sections/DescriptionSection.vue
@@ -1,8 +1,25 @@
 <template>
     <section id="descricao" class="py-16 px-8 text-center">
-        <h2 class="text-3xl font-semibold text-dark mb-6" data-aos="fade-up">O que fazemos</h2>
-        <p class="max-w-2xl mx-auto text-lg text-secondary" data-aos="fade-up" data-aos-delay="200">
-            Planejamos, criamos e gerenciamos a presença digital da sua marca com estratégias personalizadas para atrair e engajar seu público.
-        </p>
+        <div class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+            <div class="flex justify-center" data-aos="fade-up">
+                <img src="@/assets/img/handshake.png" alt="Equipe de tecnologia trabalhando"
+                class="w-full max-w-md object-cover" />
+            </div>
+
+            <div>
+                <h2 class="text-3xl font-semibold text-dark mb-6" data-aos="fade-up">O que fazemos</h2>
+                <p class="max-w-2xl mx-auto text-lg text-secondary mb-6 text-justify" data-aos="fade-up" data-aos-delay="100">
+                    Planejamos, criamos e gerenciamos a presença digital da sua marca com estratégias personalizadas para atrair e engajar seu público.
+                </p>
+                
+                <p class="max-w-2xl mx-auto text-lg text-secondary text-justify mb-6" data-aos="fade-up" data-aos-delay="200">
+                    Conectamos sua marca ao nosso hub de empresas, oferecendo soluções completas para impulsionar seu negócio e aumentar sua visibilidade no mercado.
+                </p>
+
+                <p class="max-w-2xl mx-auto text-lg text-secondary text-justify" data-aos="fade-up" data-aos-delay="200">
+                    Oferecemos soluções de multimídia para potencializar sua comunicação, incluindo produção de design gráfico e alocação de telas em eventos e pontos estratégicos.
+                </p>
+            </div>
+        </div>
     </section>
 </template>
